71c12da7f1d3c6baf5e86b122f77228b91523146
Pedro Paulo <pedropaulo062011@gmail.com>
2025-05-07 15:58:10 -0300

    Reestruturação do código e começo da exposição das sections

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
 function addSection() {
-    const caixa = document.getElementById('caixa');
+    const caixa = document.getElementById('addSBox');
+    const overlay = document.getElementById('overlay');
     if(caixa.classList.contains('hidden')) {
-        document.querySelector('#caixa').classList.add('visible');
-        document.querySelector('#caixa').classList.remove('hidden');
-        document.getElementById("overlay").classList.add("visible");
-        document.getElementById("overlay").classList.remove("hidden");
-        document.getElementById("container").style.zIndex = 0;
+        caixa.classList.replace('hidden', 'visible');
+        overlay.classList.replace('hidden', 'visible');
     } else {
-        window.alert('Uma seção já está sendo adicionada')
+        window.alert('Uma seção já está sendo adicionada');
     }
 }
+
+function closeBoxes() {
+    const caixa = document.getElementById('addSBox');
+    const overlay = document.getElementById('overlay');
+    caixa.classList.replace('visible', 'hidden');
+    overlay.classList.replace('visible', 'hidden');
+}
+
+function addS() {
+    const caixa = document.getElementById('addSBox');
+    const overlay = document.getElementById('overlay');
+    caixa.classList.replace('visible', 'hidden');
+    overlay.classList.replace('visible', 'hidden');
+    const sectionName = document.getElementById('SectionName').value;
+    let sections = JSON.parse(localStorage.getItem('sections')) || [];
+    let section = {
+        id : sections.length,
+        name : sectionName
+    }
+    sections.push(section);
+    localStorage.setItem('sections', JSON.stringify(sections));
+    
+}
+
+function updateAll() {
+    
+}
+
+document.getElementById("addSForm").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Evita qualquer ação de envio ou outra
+    }
+});
